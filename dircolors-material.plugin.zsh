a600cd77cc65c135dea74c07239379b8a18af1b5
Grigorii Horos <horosgrisa@gmail.com>
2019-04-19 23:36:35 +0300

    Add odt file format

--- dircolors-material.plugin.zsh
+++ dircolors-material.plugin.zsh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
 if (( $+commands[dircolors] )); then
-  eval $(dircolors ${${(%):-%x}:a:h}/dircolors.material )
+  eval $(dircolors ${${(%):-%x}:a:h}/material.dircolors )
 else
   if (( $+commands[gdircolors] )); then
-    eval $(gdircolors ${${(%):-%x}:a:h}/dircolors.material )
+    eval $(gdircolors ${${(%):-%x}:a:h}/material.dircolors )
   fi
 
 fi
