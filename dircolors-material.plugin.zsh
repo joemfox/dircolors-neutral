5025f2ecced140ad8ce4200cbf124b5b6683b0fb
Grigorii Horos <horosgrisa@gmail.com>
2020-03-05 14:26:53 +0200

    Change path

--- dircolors-material.plugin.zsh
+++ dircolors-material.plugin.zsh
@@ -1,17 +1,20 @@
 #!/usr/bin/env zsh
 
-# Standarized $0 handling, following:
-# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
-_DIRNAME="${0:h}"
+
 
 if [[ ! -f "${TMPDIR:-/tmp}/.material-dircolors-cache-${USER}.zsh" ]]; then
+  # Standarized $0 handling, following:
+  # https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+  0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+  _DIRNAME="${0:h}"
+  mkdir -p "${TMPDIR:-/tmp}/zsh-${UID}"
+
   if (( $+commands[dircolors] )); then
-    dircolors "${_DIRNAME}/material.dircolors" > "${TMPDIR:-/tmp}/.material-dircolors-cache-${USER}.zsh"
+    dircolors "${_DIRNAME}/material.dircolors" > "${TMPDIR:-/tmp}/zsh-${UID}/material-dircolors.zsh"
   elif (( $+commands[gdircolors] )); then
-    gdircolors "${_DIRNAME}/material.dircolors" > "${TMPDIR:-/tmp}/.material-dircolors-cache-${USER}.zsh"
+    gdircolors "${_DIRNAME}/material.dircolors" > "${TMPDIR:-/tmp}/zsh-${UID}/material-dircolors.zsh"
   fi
 fi
-source "${TMPDIR:-/tmp}/.material-dircolors-cache-${USER}.zsh"
+source "${TMPDIR:-/tmp}/zsh-${UID}/material-dircolors.zsh"
 
 export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
