bea9ecd43451095917c3ece7fddd00bac9744c2a
Grigorii Horos <horosgrisa@gmail.com>
2020-03-05 14:27:16 +0200

    Change path

--- dircolors-material.plugin.zsh
+++ dircolors-material.plugin.zsh
@@ -2,7 +2,7 @@
 
 
 
-if [[ ! -f "${TMPDIR:-/tmp}/.material-dircolors-cache-${USER}.zsh" ]]; then
+if [[ ! -f "${TMPDIR:-/tmp}/zsh-${UID}/material-dircolors.zsh" ]]; then
   # Standarized $0 handling, following:
   # https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
   0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
